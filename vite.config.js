709a0ce740b846b858a1ef70b68925d2144ffbc9
IT PARLOUR <itparlour360@gmail.com>
2026-08-14 03:49:24 +0600

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+import path from "path";
+
+function copyImgFolder() {
+  return {
+    name: "copy-img-folder",
+
+    closeBundle() {
+      const source = path.resolve("img");
+      const srouce2 = path.resolve("cv");
+      const destination = path.resolve("docs/img");
+      const destination2 = path.resolve("docs/cv");
+
+      fs.cpSync(source, destination, {
+        recursive: true,
+      });
+      fs.cpSync(srouce2, destination2, {
+        recursive: true,
+      });
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), copyImgFolder()],
   build: {
     outDir: "docs",
     emptyOutDir: true,
